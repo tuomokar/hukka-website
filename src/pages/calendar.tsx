c45a3924146b1e97a19c1acf03ae457ff500211a
Tuomo Oila <tuomo.oila@softability.fi>
2022-10-16 15:36:11 +0300

    Cancel archery for today and add information on some upcoming events

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
 import { HeadFC, Script } from 'gatsby';
-import React, { FC, ReactElement } from 'react';
+import React, { FC, ReactElement, ReactNode } from 'react';
 import { ExternalLink } from '../components/external-link';
 import { Layout } from '../components/layout'
 
 type SundayActivity = {
   date: string;
-  title: string;
+  title: ReactNode;
   additionalDescription?: string | ReactElement;
   noMeeting?: boolean;
 }
@@ -23,7 +23,7 @@ const sundayActivities: SundayActivity[] = [
   { date: '9.10.', title: 'Helmikirjonta: alkeet', },
   {
     date: '16.10.',
-    title: 'Jousiammuntaa sekä kasvivärjäys',
+    title: <>Kasvivärjäys <s>sekä jousiammuntaa</s> (jousiammunta peruttu sairaustapauksen takia)</>,
     additionalDescription: (
       <>
         <p>Kasvivärjäys on jatkoa keväällä pidetylle värjäystyöpajalle, mutta vielä ehdit mukaan!</p>
@@ -93,6 +93,61 @@ const events: Event[] = [
             Sisarseuramme Poukan (Kotkan Keskiaikaseura) järjestämä tapahtuma Pyhällön Lintukodossa. Ilmoittautuminen tapahtumaan
             avautuu 7.10. klo 18.00.
             Lisätietoa löydät mm. <ExternalLink href='https://www.facebook.com/groups/149121295016/posts/10159011887470017/'>Facebookista</ExternalLink>.
+          </span>
+        </p>
+      </>
+    )
+  },
+  {
+    date: '20.-22.1.2023',
+    title: 'Talvi-ilta',
+    description: (
+      <>
+        <p>
+          <span>
+            Sisarseuramme Humalasalon (Hämeen Keskiaika) järjestämä tapahtuma Parkanossa. Ilmoittautuminen tapahtumaan
+            avautuu 1.12.2022 klo 18.00.
+            Lisätietoa löydät mm. <ExternalLink href='http://www.talvi-ilta.humalasalo.net/'>tapahtuman kotisivuilta</ExternalLink>.
+          </span>
+        </p>
+      </>
+    )
+  },
+  {
+    date: '17.-19.2.2023',
+    title: 'Sydäntalvenjuhla',
+    description: (
+      <>
+        <p>
+          <span>
+            Sisarseuramme Unikankareen (Turun keskiaikaseura) järjestämä tapahtuma Taivalsalossa. Ilmoittautuminen tapahtumaan
+            avautuu 7.11.2022 klo 20.00.
+            Lisätietoa löydät mm. <ExternalLink href='https://keskiaikaseuraunikankare.fi/sydantalvenjuhla2023'>tapahtuman kotisivuilta</ExternalLink>.
+          </span>
+        </p>
+      </>
+    )
+  },
+  {
+    date: '14.-18.6.2023',
+    title: 'Drachenwaldin 30v juhlat',
+    description: (
+      <>
+        <p>
+          <span>
+            Euroopan kuningaskunta Drachenwaldin (johon myös Hukka ja Aarnimetsä kuuluu) järjestämä suuri juhlatapahtuma.
+            Tapahtuma järjestetään Alankomaissa Kasteel de Berckt - linnan tiloissa. Myös Suomesta on lähdössä sinne paljon porukkaa.
+          </span>
+        </p>
+        <p>
+          <span>
+            Lisätietoa löytyy mm. &nbsp;
+            <ExternalLink href='https://drachenwald.sca.org/events/calendar/#/polderslot/2023-06-14/drachenwald-s-30th-year-anniversary-celebration-and-coronation'>
+              Drachenwaldin kalenterista
+            </ExternalLink>
+            &nbsp;(tarkemmin tietoa tullee piakkoin).
+            Tapahtumaan lähteviä suomalaisia varten on myös tehty <ExternalLink href='https://www.facebook.com/groups/dw30matkalaiset/'>
+            oma ryhmänsä Facebookiin</ExternalLink>.
           </span>
         </p>
       </>
