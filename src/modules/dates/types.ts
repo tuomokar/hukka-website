b16020303df61ffa3fef1e5bafeb8312dc79afd7
Tuomo Oila <tuomo.oila@gmail.com>
2023-05-14 12:10:53 +0300

    fix: Fix typing for ISO date. Allow October as the month.

--- src/modules/dates/types.ts
+++ src/modules/dates/types.ts
@@ -1,6 +1,6 @@
 type YearDigits = `202${3 | 4}`;
 type MonthFirstDigit = 0 | 1;
-type MonthSecondDigit = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+type MonthSecondDigit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
 type DayFirstDigit = 0 | 1 | 2 | 3;
 type DaySecondDigit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
