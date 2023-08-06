59d1ff4551a51774e9de49553366eb13819179cb
Tuomo Oila <tuomo.oila@gmail.com>
2023-08-06 23:54:07 +0300

    content: added form to the Contacts page to request to be added to Hukka's information / discussion channels

--- src/pages/contact-info.tsx
+++ src/pages/contact-info.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import type { HeadFC } from "gatsby";
 import styled from "@emotion/styled";
 import { Layout } from "../components/layout";
-import { StaticImage } from "gatsby-plugin-image";
 import { ExternalLink } from "../components/external-link";
 import { Officers } from "../components/officers";
 
@@ -15,67 +14,42 @@ const ContactInfoPage = () => {
         </div>
 
         <div>
+          <InformationTitle>Jäseneksi liittyminen</InformationTitle>
+          <div>
+            <span>
+              Voit tulla mukaan kaikkeen toimintaamme myös ilman jäsenyyttä, mutta jäsenet saavat etuja
+              tapahtumamaksuissa ja jäsenmaksu tukee yhdistyksen toimintaa.
+            </span>
+            <p />
+            <span>Lisätietoa jäseneksi liittymisestä ja jäsenlomakkeen linkin löydät kattoseuramme </span>
+            <ExternalLink href="https://www.aarnimetsa.org/keskiaikaseura-yhdistyksena/jasenyys/">
+              Suomen keskiaikaseura ry:n sivuilta.
+            </ExternalLink>
+          </div>
+        </div>
+
+        <RequestFormFrame
+          id="request-form"
+          title="information-channel-join-request-form"
+          src="https://docs.google.com/forms/d/e/1FAIpQLSdOCQYNoFfpPb2GJIz-L5NtOonPhLlYCnmUr-jTRhBOR8eEJw/viewform?embedded=true"
+          width="500"
+          height="1000"
+        >
+          Loading…
+        </RequestFormFrame>
+
+        <div>
           <h3>Virkailijat</h3>
 
           <Officers />
-        </div>
-
-        <div>
-          <h3>Muuta:</h3>
-
-          <FacebookLinkContainer>
-            <ExternalLink href="https://facebook.com/groups/hukkary">
-              <>
-                <StaticImage height={30} src="../images/facebook-logo.png" alt="Facebook logo" />
-                <LinkText>Hucca Facebookissa</LinkText>
-              </>
-            </ExternalLink>
-          </FacebookLinkContainer>
-
-          <div>
-            <StaticImage height={30} src="../images/discord-logo.png" alt="Discord logo" />
-            <div>
-              <span>Pyydä kutsu esim. voudilta</span>
-            </div>
-          </div>
-
-          <div>
-            <InformationTitle>Sähköpostilista</InformationTitle>
-            <div>
-              <span>
-                Lähetä sähköposti webvastaavalle, että haluaisit listalle. Listalle ei tule spammia, vaan Hucan
-                tärkeimmät tiedotteet.
-              </span>
-            </div>
-          </div>
-
-          <div>
-            <InformationTitle>Jäseneksi liittyminen</InformationTitle>
-            <div>
-              <span>
-                Voit tulla mukaan kaikkeen toimintaamme myös ilman jäsenyyttä, mutta jäsenet saavat etuja
-                tapahtumamaksuissa ja jäsenmaksu tukee yhdistyksen toimintaa.
-              </span>
-              <p />
-              <span>Lisätietoa jäseneksi liittymisestä ja jäsenlomakkeen linkin löydät kattoseuramme </span>
-              <ExternalLink href="https://www.aarnimetsa.org/keskiaikaseura-yhdistyksena/jasenyys/">
-                Suomen keskiaikaseura ry:n sivuilta.
-              </ExternalLink>
-            </div>
-          </div>
         </div>
       </>
     </Layout>
   );
 };
 
-const FacebookLinkContainer = styled.div`
-  line-height: 30px;
-  margin-bottom: 16px;
-`;
-
-const LinkText = styled.span`
-  margin-left: 8px;
+const RequestFormFrame = styled.iframe`
+  margin: 32px 0 32px 0;
 `;
 
 const InformationTitle = styled.h3`
